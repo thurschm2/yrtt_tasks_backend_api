7a74bcb4ea45cfe8a91129484e425a56d2702f71
thurschm2 <thurschm2@gmail.com>
2021-03-11 13:48:13 +0000

    implementing environment variable for connection + fixed bug

--- src/main/java/com/techreturners/GetTasksHandler.java
+++ src/main/java/com/techreturners/GetTasksHandler.java
@@ -5,17 +5,17 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techreturners.model.Task;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -24,44 +24,51 @@ public class GetTasksHandler implements RequestHandler<APIGatewayProxyRequestEve
 
 	private static final Logger LOG = LogManager.getLogger(GetTasksHandler.class);
 
-	private Connection connection= null;
+	private Connection connection = null;
 	private PreparedStatement preparedStatement = null;
 	private ResultSet resultSet = null;
-	private Object ResultSet;
 
 	@Override
 	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
-		LOG.info("received: the request");
+		LOG.info("received the request");
+
+		String userId = request.getPathParameters().get("userId");
+
 		List<Task> tasks = new ArrayList<>();
 
-		String userId = request.getPathParameters().get("userID");
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
+
 			connection = DriverManager.getConnection(String.format("jdbc:mysql://%s/%s?user=%s&password=%s",
-					"tasks-db.cqklo55dbmqm.eu-west-2.rds.amazonaws.com",
-					"tasks",
-					"root",
-					"Mstyrs7&"));
+					System.getenv("DB_HOST"),
+					System.getenv("DB_NAME"),
+					System.getenv("DB_USER"),
+					System.getenv("DB_PASSWORD")));
 
-			preparedStatement = connection.prepareStatement( "SELECT * FROM task WHERE userID = ?");
-			preparedStatement.setString( 1, userID);
-			ResultSet = preparedStatement.executeQuery();
-			while (ResultSet.next()) {
-				Task task = new Task(ResultSet.getString('taskID'),
-						ResultSet.getString('description'),
-						ResultSet.getString('completed'));
+			preparedStatement = connection.prepareStatement("SELECT * FROM task WHERE userId = ?");
+			preparedStatement.setString(1, userId);
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				Task task = new Task(resultSet.getString("taskId"),
+						resultSet.getString("description"),
+						resultSet.getBoolean("completed"));
+
 				tasks.add(task);
-
 			}
 		}
 		catch (Exception e) {
-			LOG.error(String.format("enable to querry our database for tasks for user %s",userID, e);
+			LOG.error(String.format("Unable to query database for tasks for user %s", userId), e);
 		}
-		finally{
-			
+		finally {
+			closeConnection();
 		}
+
 		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
 		response.setStatusCode(200);
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Access-Control-Allow-Origin", "*");
+		response.setHeaders(headers);
 
 		ObjectMapper objectMapper = new ObjectMapper();
 		try {
@@ -69,9 +76,28 @@ public class GetTasksHandler implements RequestHandler<APIGatewayProxyRequestEve
 			response.setBody(responseBody);
 		}
 		catch(JsonProcessingException e) {
-			LOG.info("unable to marshall Tasks array", e);
+			LOG.error("Unable to marshall tasks array", e);
 		}
-	
+
 		return response;
+	}
+
+	private void closeConnection() {
+		try {
+			if (resultSet != null) {
+				resultSet.close();
+			}
+
+			if (preparedStatement != null) {
+				preparedStatement.close();
+			}
+
+			if (connection != null) {
+				connection.close();
+			}
+		}
+		catch (Exception e) {
+			LOG.error("Unable to close connections to MySQL - {}", e.getMessage());
+		}
 	}
 }
